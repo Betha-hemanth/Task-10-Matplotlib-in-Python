cb7e766e047362c46929d44c4f0ec84763be7dbb
BETHA HEMANTH <hemanthbetha50@gmail.com>
2025-11-05 23:11:32 +0530

    push

---
+++ 10.2.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
+temperature = [28, 30, 32, 31, 29, 27, 26]
+plt.plot(days, temperature, marker='o', linestyle='--', color='red')
+plt.xlabel('Days of the week')
+plt.ylabel('Temperature (°C)')
+plt.title('Temperature Variation Over a Week')
+plt.grid(True)
+plt.show()
